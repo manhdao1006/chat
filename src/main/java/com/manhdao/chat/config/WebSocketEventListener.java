025c7424f3549c0ebdbf790359fc4b5571bf350f
manhdao1006 <manhdao1006@gmail.com>
2024-07-02 16:25:58 +0700

    finish-disconnect-event-listener

--- src/main/java/com/manhdao/chat/config/WebSocketEventListener.java
+++ src/main/java/com/manhdao/chat/config/WebSocketEventListener.java
@@ -1,8 +1,13 @@
 package com.manhdao.chat.config;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.manhdao.chat.controller.ChatMessage;
+import com.manhdao.chat.controller.MessageType;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +17,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                                        .type(MessageType.LEAVE)
+                                        .sender(username)
+                                        .build();
+
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
     }
 
 }
